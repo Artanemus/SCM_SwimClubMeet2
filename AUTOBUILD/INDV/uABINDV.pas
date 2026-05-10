2c5576e69cace1cdc289a114284eafb62a3564ca
Artanemus <69775305+Artanemus@users.noreply.github.com>
2026-05-10 12:18:35 +1000

    Update uABINDV.pas

--- AUTOBUILD/INDV/uABINDV.pas
+++ AUTOBUILD/INDV/uABINDV.pas
@@ -4,6 +4,7 @@ interface
 
 uses
   System.SysUtils, System.Classes, System.UITypes, System.Math,
+  System.StrUtils, System.Types,
   VCL.Dialogs,
   FireDAC.Comp.Client, FireDAC.Stan.Error,
   dmSCM2, dmCORE, uSettings,
@@ -18,6 +19,8 @@ type
     fVerbose: boolean;
     fError: boolean;
     fErrorNum: integer;
+    LaneBin: Array of array of integer; // two dimensional array [heat][lane]
+
     function CalcNumberOfHeats(NumOfNominees: Integer): Integer;
 
   public
@@ -49,7 +52,7 @@ end;
 function TABINDV.AutoBuildExec: Boolean;
 var
   msg: string;
-  totalNumOfHeats, I, indx, fNumOfHeats: Integer;
+  totalHeatCount, fHeatNum,J, indx, fHeatCount, fLaneCount: Integer;
 begin
   result := false;
 
@@ -114,9 +117,8 @@ begin
     ABData.procRenumberHeats.ExecProc;
   end;
 
-
   // There must be a least 2 lanes for the scatter algorithm.
-  if (uSwimClub.NumberOfLanes < 2) then
+  if ( uSwimClub.NumberOfLanes < 2) then
   begin
     if (fVerbose) then
     begin
@@ -184,17 +186,123 @@ begin
   if (Settings.ab_SeperateGender = false)
     and (Settings.ab_GroupByIndx <= 0) then
   begin
-      fNumOfHeats := CalcNumberOfHeats(ABData.qryUnplacedNominees.RecordCount);
       ABData.qryUnplacedNominees.IndexName := 'indxTTB';
       if ABData.qryUnplacedNominees.Filtered then
         ABData.qryUnplacedNominees.Filtered := false;
 
-      for I := 0 to fNumOfHeats-1 do
+      // CALCRealLaneCount...
+      // Are lanes being excluded?
+      fLaneCount := uSwimClub.NumberOfLanes;
+      if Settings.ab_ExcludeOutsideLanes then
+      fLaneCount := fLaneCount - 2;
+      if Settings.ab_ExcludeLanesCustom then
       begin
-        uHeat.NewHeat; // Builds the heat and lanes
-        // uUtility.ScatterLanes(indx, uSwimClub.NumberOfLanes)  // Scatter....
-        // place the nominee into the lane...
+        var sa: TStringDynArray;
+        sa := SplitString(Settings.ab_ListOfExcludeLanes, ',');
+        for J := Low(sa) to High(sa) do
+        begin
+          var s:string;
+          var LaneNum: integer;
+          s := sa[J];
+          s := Trim(s);
+          LaneNum := StrToIntDef(s, 0);
+          if LaneNum > 0 then
+          begin
+            if Settings.ab_ExcludeOutsideLanes then
+            begin
+              if (LaneNum <> 1) and (LaneNum <> uSwimClub.NumberOfLanes) then
+                fLaneCount := fLaneCount - 1;
+            end
+            else
+              fLaneCount := fLaneCount - 1;
+          end;
+        end;
       end;
+
+      if (fLaneCount <= 0) then
+      begin
+        if (fVerbose) then
+        begin
+          msg := '''
+            After taking into consideration "exclude outside lanes"
+            and the user''s selected lanes to exclude, the total number
+            of lanes available for a heat is zero. Consider making
+            adjustment to the auto-build settings.
+            Auto-Build will exit.
+            ''';
+          MessageDlg(msg, mtError, [mbOK], 0, mbOK);
+        end;
+        // Is this an error or a warning.
+        fError := true;
+        fErrorNum := 1;
+        result := false;
+        exit;
+      end;
+
+
+      fHeatCount := CalcNumberOfHeats(ABData.qryUnplacedNominees.RecordCount);
+
+      ABData.qryUnplacedNominees.First;
+      Setlength(LaneBin, fHeatCount, fLaneCount);
+      fHeatNum := 0;
+
+      // CIRCLE SEEDING ...
+      if Settings.ab_SeedMethodIndx = 1 then
+      BEGIN
+        var fSeedDepth: integer;
+        fSeedDepth := Settings.ab_SeedDepth;
+        if (fSeedDepth <> 0) and (fHeatCount >= fSeedDepth) then
+        begin
+          // Nominees are Sorted on fastest to slowest TTB.
+          // Seed depth is fHeatCount.
+          // LaneCount is SwimClub.NumOfLanes - (Exclude gutter lanes + Exclude custom lanes.)
+          var LaneNum: integer;
+          LaneNum := 0;
+          // keep repeating loop across heat while...
+          while not ABData.qryUnplacedNominees.EOF and (LaneNum < fSeedDepth) do
+          begin
+            for fHeatNum := 0 to fSeedDepth-1 do // cloop across heats..
+            begin
+              if ABData.qryUnplacedNominees.EOF then break;
+              // .. seeding nominee swimmers into heats
+              LaneBin[fHeatNum][LaneNum] := ABData.qryUnplacedNominees.FieldByName('NomineeID').AsInteger;
+              ABData.qryUnplacedNominees.Next;
+            end;
+            Inc(LaneNum, 1);
+          end;
+          fHeatCount := fHeatCount - fSeedDepth;
+        end;
+      END;
+
+
+      // DEFAULT SEEDING ....
+      if fHeatNum < fHeatCount-1 then
+      begin
+        // complete seeding the remain lanes using default method
+          var LaneNum: integer;
+          LaneNum := 0;
+          // keep repeating loop across heat while...
+          while not ABData.qryUnplacedNominees.EOF and (LaneNum < fHeatCount) do
+          begin
+            for fHeatNum := 0 to fHeatCount-1 do // cloop across heats..
+            begin
+              var I: integer;
+              for I := 0 to fLaneCount-1 do
+              begin
+                if ABData.qryUnplacedNominees.EOF then break;
+                // .. seeding nominee swimmers into heats
+                LaneBin[fHeatNum][I] := ABData.qryUnplacedNominees.FieldByName('NomineeID').AsInteger;
+                ABData.qryUnplacedNominees.Next;
+              end;
+            end;
+            Inc(LaneNum, 1);
+          end;
+      end;
+
+
+
+
+
 
   end;
 
@@ -202,7 +310,7 @@ begin
   if (Settings.ab_SeperateGender = true)
     and (Settings.ab_GroupByIndx <= 0) then
   begin
-    totalNumOfHeats := 0;
+    totalHeatCount := 0;
     ABData.qryUnplacedNominees.IndexName := 'indxTTB';
     if not ABData.qryUnplacedNominees.Filtered then
       ABData.qryUnplacedNominees.Filtered := true;
@@ -215,10 +323,10 @@ begin
         var id: integer;
         id := ABData.qryGender.FieldByName('GenderID').AsInteger;
         ABData.qryUnplacedNominees.Filter := 'GenderID = ' + IntToStr(id); // gender M.
-        fNumOfHeats := CalcNumberOfHeats(ABData.qryUnplacedNominees.RecordCount);
-        totalNumOfHeats := totalNumOfHeats + fNumOfHeats;
+        fHeatCount := CalcNumberOfHeats(ABData.qryUnplacedNominees.RecordCount);
+        totalHeatCount := totalHeatCount + fHeatCount;
 
-        for I := 0 to fNumOfHeats-1 do
+        for fHeatNum := 0 to fHeatCount-1 do
         begin
 //          uHeat.NewHeat; // Builds the heat and lanes
           // uUtility.ScatterLanes(indx, uSwimClub.NumberOfLanes)  // Scatter....
